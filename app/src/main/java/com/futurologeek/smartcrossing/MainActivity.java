2c4dc93f6f93b79b1c8bfc45967b8f0cb428068d
Arus <a.mirecki99@gmail.com>
2016-10-16 20:52:04 +0200

    Main Activity is now responsible, added popup xml

--- app/src/main/java/com/futurologeek/smartcrossing/MainActivity.java
+++ app/src/main/java/com/futurologeek/smartcrossing/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
-                Toast.makeText(MainActivity.this, "Dialog", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Dodaj", Toast.LENGTH_SHORT).show();
             }
         });
         settings.setOnClickListener(new View.OnClickListener() {
@@ -65,29 +65,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Place plejs = new Place("Trzemeska", 5);
         adapter=new PlacesAdapter(this, listItems);
         booklist.setAdapter(adapter);
-        listItems.add(new Place("Trzemeska",5));
-        listItems.add(new Place("Wiśniowa",4));
-        listItems.add(new Place("Mazowiecka",8));
-        listItems.add(new Place("Gajowa",14));
+        listItems.add(new Place("Wiśniowa",4, 51.0844578,17.0234934));
+        listItems.add(new Place("Mazowiecka",8, 51.1070619,17.0481559));
+        listItems.add(new Place("Gajowa",14,51.0924032,17.0384109));
 
-        listItems.add(new Place("Stanisława",5));
-        listItems.add(new Place("Piękna",4));
-        listItems.add(new Place("Kasztanowa",8));
-        listItems.add(new Place("Powstańców",14));
+        listItems.add(new Place("Stanisława",5, 51.0986663,17.0390955));
+        listItems.add(new Place("Piękna",4,51.0849709,17.0522465));
+        listItems.add(new Place("Kasztanowa",8,51.0844352,17.0138016));
+        listItems.add(new Place("Powstańców",14,51.0893211,17.0138097));
 
-        listItems.add(new Place("3 maja",5));
-        listItems.add(new Place("Karmazynowa",4));
-        listItems.add(new Place("Orląt",8));
-        listItems.add(new Place("Wyspiańskiego",14));
-
-        listItems.add(new Place("Niemcewicza",5));
-        listItems.add(new Place("Norwida",4));
-        listItems.add(new Place("Grabiszyńska",8));
-        listItems.add(new Place("Tęczowa",14));
-
+        listItems.add(new Place("Orląt",8,51.1077513,17.0177393));
+        listItems.add(new Place("Wyspiańskiego",14, 51.1071437,17.0588681));
         adapter.notifyDataSetChanged();
 
     }
