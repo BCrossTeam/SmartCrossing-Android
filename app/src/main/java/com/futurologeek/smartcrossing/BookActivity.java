c88f5d67a70fec3d14e2de37dd3b1eb25a9e024c
Arus <a.mirecki99@gmail.com>
2016-10-21 18:27:29 +0400

    BookActivity is now showing creator username instead of user id

--- app/src/main/java/com/futurologeek/smartcrossing/BookActivity.java
+++ app/src/main/java/com/futurologeek/smartcrossing/BookActivity.java
@@ -24,6 +24,7 @@ public class BookActivity extends AppCompatActivity {
     private TextView titleTextView, authorTextView, dateTextView, isbnTextView, addedByTextView, categoryTextView;
     private RelativeLayout visitProfile;
     int id;
+    int creator_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +37,7 @@ public class BookActivity extends AppCompatActivity {
         if(getIntent().getExtras()!=null) {
             Bundle przekazanedane = getIntent().getExtras();
             id = przekazanedane.getInt("ajdi");
-            new getUserBooks().execute();
+            new getUserBooks(true).execute();
         }
     }
 
@@ -49,7 +50,16 @@ public class BookActivity extends AppCompatActivity {
         addedByTextView= (TextView) findViewById(R.id.added_by_textview);
         isbnTextView= (TextView) findViewById(R.id.isbn_textview);
     }
+
+
     class getUserBooks extends AsyncTask<Void, Void, Void> {
+        Boolean getList;
+
+
+        public getUserBooks(Boolean getList){
+            super();
+            this.getList = getList;
+        }
 
         @Override
         protected void onPreExecute() {
@@ -59,54 +69,70 @@ public class BookActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... arg0) {
             HttpHandler sh = new HttpHandler();
+            String jsonStr;
+            if(getList){
+                 jsonStr = sh.makeServiceCall(Constants.book_url+id);
+            } else {
+                jsonStr = sh.makeServiceCall(Constants.user_url+creator_id);
+            }
 
-            // Making a request to url and getting response
-            String jsonStr = sh.makeServiceCall(Constants.book_url+id);
 
             Log.e("tag", "Response from url: " + jsonStr);
 
             if (jsonStr != null) {
                 try {
-                    JSONObject jsonObj = new JSONObject(jsonStr);
-                    final String title = jsonObj.getString("book_title");
-                    final int id = jsonObj.getInt("book_id");
-                    final String author = jsonObj.getString("book_author");
-                    final String ISBN = jsonObj.getString("book_isbn");
-                    final String pub_date = jsonObj.getString("book_publication_date");
-                    final String cat = jsonObj.getString("book_category");
-                    final int creator_id = jsonObj.getInt("book_user_author");
+                    if(getList){
+                        JSONObject jsonObj = new JSONObject(jsonStr);
+                        final String title = jsonObj.getString("book_title");
+                        final int id = jsonObj.getInt("book_id");
+                        final String author = jsonObj.getString("book_author");
+                        final String ISBN = jsonObj.getString("book_isbn");
+                        final String pub_date = jsonObj.getString("book_publication_date");
+                        final String cat = jsonObj.getString("book_category");
+                        creator_id = jsonObj.getInt("book_user_author");
 
                         Book ksiazka = new Book(id, title, author);
 
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            titleTextView.setText(title);
-                            authorTextView.setText(author);
-                            categoryTextView.setText(getResources().getString(R.string.cat)+" "+cat);
-                            dateTextView.setText(getResources().getString(R.string.year)+" "+pub_date);
-                            isbnTextView.setText(getResources().getString(R.string.isbn)+" "+ISBN);
-                            addedByTextView.setText(getResources().getString(R.string.added_by)+" "+creator_id);
-                            visitProfile.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    if (NetworkStatus.checkNetworkStatus(BookActivity.this)) {
-                                        Intent i = new Intent(BookActivity.this, ProfileActivity.class);
-                                        //Todo: Pobieranie user id
-                                        Bundle koszyk = new Bundle();
-                                        koszyk.putString("u_id", String.valueOf(creator_id));
-                                        i.putExtras(koszyk);
-                                        startActivity(i);
-                                        finish();
-                                    } else {
-                                        Toast.makeText(BookActivity.this, getResources().getString(R.string.no_network), Toast.LENGTH_LONG).show();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                titleTextView.setText(title);
+                                authorTextView.setText(author);
+                                categoryTextView.setText(getResources().getString(R.string.cat)+" "+cat);
+                                dateTextView.setText(getResources().getString(R.string.year)+" "+pub_date);
+                                isbnTextView.setText(getResources().getString(R.string.isbn)+" "+ISBN);
+                                visitProfile.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        if (NetworkStatus.checkNetworkStatus(BookActivity.this)) {
+                                            Intent i = new Intent(BookActivity.this, ProfileActivity.class);
+                                            //Todo: Pobieranie user id
+                                            Bundle koszyk = new Bundle();
+                                            koszyk.putString("u_id", String.valueOf(creator_id));
+                                            i.putExtras(koszyk);
+                                            startActivity(i);
+                                            finish();
+                                        } else {
+                                            Toast.makeText(BookActivity.this, getResources().getString(R.string.no_network), Toast.LENGTH_LONG).show();
+                                        }
+
                                     }
+                                });
+                                new getUserBooks(false).execute();
+                            }
+                        });
 
-                                }
-                            });
-                        }
-                    });
+                    } else {
+                        JSONObject jsonObj = new JSONObject(jsonStr);
+                        final String uname = jsonObj.getString("user_name");
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                              addedByTextView.setText(getResources().getString(R.string.added_by)+" "+uname);
+                            }
+                        });
 
+                    }
                 } catch (final JSONException e) {
                     Log.e("TAG", "Json parsing error: " + e.getMessage());
                     runOnUiThread(new Runnable() {
